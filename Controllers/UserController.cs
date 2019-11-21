947c0f4a4753fe3f069750725c79c81666f73b5b
Eric Solari <sliceofbytes@gmail.com>
2019-11-21 16:57:07 -0600

    Initial Commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace BlazorCookieAuth.Controllers
+{
+
+	[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        // /api/User/GetUser
+        [HttpGet("[action]")]
+        public UserModel GetUser()
+        {
+            // Instantiate a UserModel
+            var userModel = new UserModel
+            {
+                UserName = "[]",
+                IsAuthenticated = false
+            };
+            // Detect if the user is authenticated
+            if (User.Identity.IsAuthenticated)
+            {
+                // Set the username of the authenticated user
+                userModel.UserName =
+                    User.Identity.Name;
+                userModel.IsAuthenticated =
+                    User.Identity.IsAuthenticated;
+            };
+            return userModel;
+        }
+    }
+    // Class to hold the UserModel
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public bool IsAuthenticated { get; set; }
+
+		public string Token { get; set; }
+    }
+}
